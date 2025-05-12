c8db8ecd57fa180b72c75b02af6593857756faf6
Zindriana <hulten.johan22@gmail.com>
2025-05-12 20:50:19 +0200

    refactored the structure

--- app/src/main/java/skalman/MainActivity.kt
+++ app/src/main/java/skalman/MainActivity.kt
@@ -1,5 +1,15 @@
 package skalman
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import skalman.skalman.ui.theme.SkalmanTheme
+import skalman.ui.calendar.CalendarScreen
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
