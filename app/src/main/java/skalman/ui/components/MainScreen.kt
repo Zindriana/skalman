bffba7751a17114b6d45efac70e0d9b65a16e07a
Zindriana <hulten.johan22@gmail.com>
2025-05-13 14:54:26 +0200

    created a mainScreen component

---
+++ app/src/main/java/skalman/ui/components/MainScreen.kt
@@ -0,0 +1,16 @@
+package skalman.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import skalman.ui.calendar.CalendarScreen
+
+@Composable
+fun MainScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CalendarScreen()
+        DateTimeDisplay(modifier = Modifier.align(Alignment.TopEnd))
+    }
+}
