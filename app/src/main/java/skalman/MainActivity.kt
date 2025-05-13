106a305b8941fdf9f5da39342272fdddec11d916
Zindriana <hulten.johan22@gmail.com>
2025-05-13 15:36:50 +0200

    added MainScreen() to the MainActivity and hide the statusbar

--- app/src/main/java/skalman/MainActivity.kt
+++ app/src/main/java/skalman/MainActivity.kt
@@ -1,27 +1,41 @@
 package skalman
 
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import skalman.skalman.ui.theme.SkalmanTheme
-import skalman.ui.calendar.CalendarScreen
+import skalman.ui.components.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Aktivera immersive mode
+        hideSystemUI()
+
+        // Ladda Compose-innehåll
         setContent {
-            SkalmanTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    CalendarScreen()
-                }
+            MainScreen()
+        }
+    }
+
+    private fun hideSystemUI() {
+        // Gör så layout kan ligga bakom systemfält
+        window.setDecorFitsSystemWindows(false)
+
+        // Dölj status- och navigationsfält
+        window.decorView.post {
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
     }
 }
