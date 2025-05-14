ae0f408aa1499668eb1065ff7f011ca73bb10615
Zindriana <hulten.johan22@gmail.com>
2025-05-14 14:39:47 +0200

    created DayCard

---
+++ app/src/main/java/skalman/ui/calendar/DayCard.kt
@@ -0,0 +1,38 @@
+package skalman.ui.calendar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import skalman.data.models.CalendarAlarm
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DayCard(alarms: List<CalendarAlarm>) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Din kalender") }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            LazyColumn {
+                items(alarms) { alarm ->
+                    AlarmCard(alarm)
+                }
+            }
+        }
+    }
+}
