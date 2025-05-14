8b16487db20b3e802650a1137199b80f4b595138
Zindriana <hulten.johan22@gmail.com>
2025-05-14 11:19:54 +0200

    cleaned some comments

--- app/src/main/java/skalman/MainActivity.kt
+++ app/src/main/java/skalman/MainActivity.kt
@@ -10,21 +10,13 @@ import skalman.ui.components.MainScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Aktivera immersive mode
         hideSystemUI()
-
-        // Ladda Compose-innehåll
         setContent {
             MainScreen()
         }
     }
 
     private fun hideSystemUI() {
-        // Gör så layout kan ligga bakom systemfält
-        window.setDecorFitsSystemWindows(false)
-
-        // Dölj status- och navigationsfält
         window.decorView.post {
             window.insetsController?.let { controller ->
                 controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
