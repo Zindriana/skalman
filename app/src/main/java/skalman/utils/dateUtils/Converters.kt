5d223fbbabd0b5ac41a22f78cbce5f1f17fed67a
Zindriana <hulten.johan22@gmail.com>
2025-05-16 18:00:24 +0200

    added a type converter

---
+++ app/src/main/java/skalman/utils/dateUtils/Converters.kt
@@ -0,0 +1,17 @@
+package skalman.util
+
+import androidx.room.TypeConverter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object Converters {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @TypeConverter
+    fun fromString(value: String?): LocalDateTime? =
+        value?.let { LocalDateTime.parse(it, formatter) }
+
+    @TypeConverter
+    fun toString(dateTime: LocalDateTime?): String? =
+        dateTime?.format(formatter)
+}
