9ac76aeb87a616c2448456c969d399fff85dad6f
Zindriana <hulten.johan22@gmail.com>
2025-05-20 15:38:12 +0200

    added DateViewButton to main screen

--- app/src/main/java/skalman/ui/components/MainScreen.kt
+++ app/src/main/java/skalman/ui/components/MainScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import skalman.ui.calendar.CalendarScreen
+import skalman.ui.calendar.DateViewButton
 
 @Composable
 fun MainScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
         CalendarScreen()
+        DateViewButton()
         DateTimeDisplay(modifier = Modifier.align(Alignment.TopEnd))
     }
 }
