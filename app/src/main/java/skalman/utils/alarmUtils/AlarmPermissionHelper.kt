eb13c8dcc4e95e04d0534f33fc3470e3f963e479
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2025-05-28 12:52:45 +0200

    Fixed the date and time picker and created a helper that requests permission from the user to be able to add exact alarm time

---
+++ app/src/main/java/skalman/utils/alarmUtils/AlarmPermissionHelper.kt
@@ -0,0 +1,23 @@
+package skalman.utils.alarmUtils
+
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.core.net.toUri
+
+object AlarmPermissionHelper {
+
+    fun checkAndRequestExactAlarmPermission(context: Context): Boolean {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        if (!alarmManager.canScheduleExactAlarms()) {
+            val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                data = "package:${context.packageName}".toUri()
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            context.startActivity(intent)
+            return false
+        }
+        return true
+    }
+}
